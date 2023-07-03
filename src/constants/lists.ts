d04fe1f4489ae758637ca5c11dd5cde2bd26b85b
wuzhenbin <1137057407@qq.com>
2023-07-03 15:52:10 +0800

    feat: update list

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,7 +1,7 @@
 // the Uniswap Default token list lives here
 // export const DEFAULT_TOKEN_LIST_URL = 'tokens.uniswap.eth'
 export const DEFAULT_TOKEN_LIST_URL =
-  'https://raw.githubusercontent.com/MXCzkEVM/wannseeswap-tokenlist/main/tokenlist.json?version=5'
+  'https://raw.githubusercontent.com/MXCzkEVM/wannseeswap-tokenlist/main/tokenlist.json?version=7'
 // https://raw.githubusercontent.com/compound-finance/token-list/master/compound.tokenlist.json
 
 // export const DEFAULT_LIST_OF_LISTS: string[] = [
